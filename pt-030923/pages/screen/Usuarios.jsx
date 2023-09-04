8c892931fb5894ae796be59b3fe2b7d69c2f605f
Aluon <paulacanoroman@gmail.com>
2023-09-04 10:49:45 +0200

    feat: llamadaAPI

--- pt-030923/pages/screen/Usuarios.jsx
+++ pt-030923/pages/screen/Usuarios.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 
 const Usuarios = () => {
 
+    const [datosAPI, setDatosAPI] = useState([])
+
+    useEffect(() => {
+        llamadaAPI()
+    }, [])
+
+    const llamadaAPI = async () => {
+        try{
+            const llamada = await fetch(`https://randomuser.me/api/?results=50`);
+            const datos = await llamada.json();
+            console.log(datos.results);
+            setDatosAPI(datos.results)
+        }catch(error) {"error detected", error}
+    }
     
+
   return (
     <div>Usuarios</div>
   )
